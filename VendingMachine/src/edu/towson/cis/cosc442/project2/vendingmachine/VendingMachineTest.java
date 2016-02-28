e6f377dde947c7bfc783e8518f009b01b29038c6
ncanno1 <ncanno1@students.towson.edu>
2016-02-28 15:31:54 -0500

    Add tests for adding item into each slot

--- VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
+++ VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
@@ -10,6 +10,8 @@ public class VendingMachineTest {
 	
 	VendingMachine vm1;
 	VendingMachine vm2;
+	VendingMachineItem vmi;
+	boolean exceptionThrown;
 
 	@Before
 	public void setUp() throws Exception {
@@ -19,17 +21,111 @@ public class VendingMachineTest {
 		vm2.addItem(new VendingMachineItem("item2", 1.25), "B");
 		vm2.addItem(new VendingMachineItem("item3", 1.50), "C");
 		vm2.addItem(new VendingMachineItem("item1", 1.75), "D");
+		vmi = new VendingMachineItem("item", 2.00);
+		exceptionThrown = false;
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		vm1 = null;
 		vm2 = null;
+		vmi = null;
 	}
 
+	/*
+	 * Testing adding item into slot A
+	 * Should add the item to the specified slot
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddItemA() {
+		vm1.addItem(vmi, "A");
+		assertEquals(vmi, vm1.getItem("A"));
+	}
+	
+	/*
+	 * Testing adding item into slot B
+	 * Should add the item to the specified slot
+	 */
+	@Test
+	public void testAddItemB() {
+		vm1.addItem(vmi, "B");
+		assertEquals(vmi, vm1.getItem("B"));
+	}
+	
+	/*
+	 * Testing adding item into slot C
+	 * Should add the item to the specified slot
+	 */
+	@Test
+	public void testAddItemC() {
+		vm1.addItem(vmi, "C");
+		assertEquals(vmi, vm1.getItem("C"));
+	}
+	
+	/*
+	 * Testing adding item into slot D
+	 * Should add the item to the specified slot
+	 */
+	@Test
+	public void testAddItemD() {
+		vm1.addItem(vmi, "D");
+		assertEquals(vmi, vm1.getItem("D"));
+	}
+	
+	/*
+	 * Testing adding item into slot A when full
+	 * Should throw an exception
+	 */
+	@Test
+	public void testAddItemAFull() {
+		try {
+			vm2.addItem(vmi, "A");
+		} catch (VendingMachineException e) {
+			exceptionThrown = true;
+		}
+		assertTrue(exceptionThrown);
+	}
+	
+	/*
+	 * Testing adding item into slot B when full
+	 * Should throw an exception
+	 */
+	@Test
+	public void testAddItemBFull() {
+		try {
+			vm2.addItem(vmi, "B");
+		} catch (VendingMachineException e) {
+			exceptionThrown = true;
+		}
+		assertTrue(exceptionThrown);
+	}
+	
+	/*
+	 * Testing adding item into slot C when full
+	 * Should throw an exception
+	 */
+	@Test
+	public void testAddItemCFull() {
+		try {
+			vm2.addItem(vmi, "C");
+		} catch (VendingMachineException e) {
+			exceptionThrown = true;
+		}
+		assertTrue(exceptionThrown);
+	}
+	
+	/*
+	 * Testing adding item into slot D when full
+	 * Should throw an exception
+	 */
+	@Test
+	public void testAddItemDFull() {
+		try {
+			vm2.addItem(vmi, "D");
+		} catch (VendingMachineException e) {
+			exceptionThrown = true;
+		}
+		assertTrue(exceptionThrown);
 	}
 
 }
